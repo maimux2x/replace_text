c677462a18d1d69d13d96372b88f6de06dc47d9b
maimu <m0smile10often@gmail.com>
2023-08-27 22:51:07 +0900

    メソッド分割をした

--- replace_text.rb
+++ replace_text.rb
@@ -1,16 +1,29 @@
 require "git"
-target_text = ARGV
-dir_current = Dir.getwd
-git_client = Git.open(dir_current)
-target_text_before = target_text[0]
-target_text_after = target_text[1]
-git_client.grep(target_text_before).each do |file|
-  file[0].scan(/\b:[^;]+/).map do |s|
-    target_file = s.split(':').last
-    target_file_text = File.open(target_file, "r") { |f| f.read() }
 
-    target_file_text.gsub!(target_text_before, target_text_after)
-    pp target_file_text
-    File.open(target_file, "w") { |f| f.write(target_file_text) }
+def current_dirctory
+  Dir.getwd
+end
+
+def repository
+  Git.open(current_dirctory)
+end
+
+def replace_text(target_texts)
+  target_text_before = target_texts[0]
+  target_text_after = target_texts[1]
+
+  git_repository = repository
+
+  git_repository.grep(target_text_before).each do |results|
+    results[0].scan(/\b:[^;]+/).each do |result|
+      target_file = result.split(':').last
+      target_file_text = File.open(target_file, "r") { |f| f.read() }
+
+      target_file_text.gsub!(target_text_before, target_text_after)
+      File.open(target_file, "w") { |f| f.write(target_file_text) }
+    end
   end
 end
+
+target_texts = ARGV
+replace_text(target_texts)
